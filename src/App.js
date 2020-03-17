5324a9b6343212b68bc2aa8c65486c2ddd48bb45
Julio López <ju.lopez73@gmail.com>
2020-03-17 19:24:30 +0000

    Código realizado en la clase 3 del curso

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React from 'react';
 import './styles/styles.scss';
 import Curso from './Curso'
 
+const cursos = [
+  {
+    "titulo": "React desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20Rutas%20manejo%20de%20estados%20%281%29.png",
+    "price": 40,
+    "profesor": "Beto Quiroga"
+
+  },
+  {
+    "titulo": "Drupal desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/drupal-poster-720_3.jpg?itok=e93ErhMN",
+    "price": 30,
+    "profesor": "Beto Quiroga"
+  },
+  {
+    "titulo": "Go desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-03/Go-desde-cero.png",
+    "price": 50,
+    "profesor": "Alexis Lozada"
+  },
+  {
+    "titulo": "HTML desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/HTML-2018.jpg?itok=Gyvm-W9t",
+    "price": 10,
+    "profesor": "Alvaro Felipe"
+  }
+]
+
 const App = () => (
   <>
     <div className="main-banner img-container l-section" id="main-banner">
@@ -17,21 +45,9 @@ const App = () => (
       </div>
     </div>
     <div className="ed-grid m-grid-3">
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
-      <Curso />
+      {
+        cursos.map( c => <Curso title={c.titulo} image={c.image} price= {c.price} profesor={c.profesor} /> )
+      }
     </div>
 </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
 
-const Curso = () => (
+const Curso = ({title, image, price, profesor}) => (
   <article className="card">
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-03/Go-desde-cero.png" alt="Curso go" />
+      <img src={image} alt={title} />
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-      </h3>
-      <div className="s-mb-2 s-main-center">
-        <div className="card__teacher s-cross-center">
-          <div className="card__avatar s-mr-1">
-            <div className="circle img-container">
-              <img src="https://api.ed.team/files/avatars/6dbe3f62-f119-46b7-8d50-632c986c0d3a.jpg" alt="Profesor" />
-            </div>
-          </div>
-          <span className="small">Alexys Lozada</span>
-        </div>
+      <h3 className="t5 s-mb-2 s-center"> {title} </h3>
+      <div className="s-main-center">
+        {profesor}
       </div>
       <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="https://ed.team">$ 20USD</a>
+    <a className="button--ghost-alert button--tiny" href="https://ed.team">{ `$ ${price} UDS` }</a>
       </div>
     </div>
   </article>
 )
+
+
+
+Curso.defaultProps = {
+  title: 'No se encontró ningún título',
+  image: "https://c.pxhere.com/photos/27/9a/architecture_buildings_city_cityscape_downtown_hd_wallpaper_reflection_river-1170115.jpg!d",
+  price: "--",
+  Profesor: ""
+}
 
 export default Curso
