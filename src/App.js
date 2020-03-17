8d013e8165789245d9c15f3521a4d1b4694558ee
Julio López <ju.lopez73@gmail.com>
2020-03-17 16:10:27 +0000

    Primer commit del curso React.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" src="https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="banner" />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Cursos de EDteamr</p>
+            <p> Tu futuro te está esperando</p>
+            <a href="https://ed.team" className="button">Suscribirse</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+</>
+)
 
 export default App;
+
+// Reglas JSX
+// 1) Toda etiqueta debe cerrarse.
+// 2) Los componentes deben de devolver UN solo elemento padre.
+// 3) Apoyarse de los Fragment cuando necesito devolver 2 o más elementos.
+// 4) Monenclatrura Fragment o =>  <> hijos </>
+// 5) img siempre se cierra.
+// 6) Nombramiento de clases: class pasa a ser => className.
+// 7) El atributo for, pasa a ser => htmlFor
+// 8) En JSX NO se pueden utilizar las condicionales if, else, while.ººººººººººººººº
