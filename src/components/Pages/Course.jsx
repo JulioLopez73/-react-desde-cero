3dc1718a1be93883e9cb9d0961ae7d9a4b93fe10
Julio López <ju.lopez73@gmail.com>
2020-03-29 17:37:01 +0100

    Código de la cuarta clase del segundo curso utilizando el hook useEffect

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -1,23 +1,19 @@
- import React, {useState} from 'react'
+ import React, {useState, useEffect} from 'react'
+ import axios from 'axios'
 
 const Course = ({match}) => {
 
-  const [state, setState] = useState({
-        id:1,
-        titulo: "React desde cero",
-        image: "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20Rutas%20manejo%20de%20estados%20%281%29.png",
-        price: 40,
-        profesor: "Beto Quiroga"
-  })
+  const [state, setState] = useState({})
 
+  useEffect(() => {
+    console.log("hola")
+    axios.get("http://my-json-server.typicode.com/JulioLopez73/json-db/cursos/4")
+    .then(resp => setState(resp.data))
+  }, [])
+
+  
   // const cursoActual = cursos.filter(c => c.id === parseInt(match.params.id))[0]
 
-const changeTitle = (text) => {
-  setState({
-    ...state,
-    titulo: text
-  })
-}
 
   return (
     <div className="ed-grid m-grid-3">
@@ -27,7 +23,6 @@ const changeTitle = (text) => {
             <h1 className="m-cols-3">{state.titulo}</h1>
             <img src={state.image} alt={state.titulo} className="m-cols-1"/>
             <p className="m-cols-2">Profesor: {state.profesor}</p>
-            <button onClick={changeTitle.bind(this, "Go desde cero")}>Cambiar título</button>
           </>
         ) :
           <h1>El curso no existe</h1>
